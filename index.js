79e2431c09698dc1f439e990e331c705c3cc4b6f
AtifUsmani <73231217+AtifUsmani@users.noreply.github.com>
2021-12-10 06:56:33 +0530

    Connected to Frontend

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ app.get("/", (req, res) => {
     res.send('Hello, world!');
 })
 
+io.on("connection", socket => {
+    console.log("Someone connected!");
+    socket.on("join-room", ({ roomId, userName })=>{
+        console.log("User joined the room!");
+        console.log(roomId);
+        console.log(userName);
+    })
+})
+
 server.listen(port, ()=> {
     console.log('Zoom clone API server listening on localhost:3001');
 });
